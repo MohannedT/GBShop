d49664da77781df057782e92d7a6f58b91e6e0b3
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2021-02-09 13:27:09 +0300

    init

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Александр Ипатов on 09.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
